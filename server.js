331ecfeea65e7e7356129745c00050011e960188
Aviral55 <messichips10@gmail.com>
2025-03-02 20:59:43 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+app.use(cors());
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("message", (data) => {
+    io.emit("message", data); // Send message to all clients
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
